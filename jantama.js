a1e829ae9eea34fa841be042df33e0bcf46356e5
tomaru919 <156040562+tomaru919@users.noreply.github.com>
2024-05-03 12:15:51 +0900

    Add files via upload

---
+++ jantama.js
@@ -0,0 +1,185 @@
+const charList = [{name: "藤田佳奈", have: false}, {name: "三上千織", have: false}, {name: "相原舞", have: false}, {name: "撫子", have: false},
+{name: "八木唯", have: false}, {name: "九条璃雨", have: false}, {name: "ジニア", have: false}, {name: "カーヴィ", have: false},
+{name: "サラ", have: false}, {name: "二之宮花", have: false}, {name: "白石奈々", have: false}, {name: "小鳥遊雛田", have: false},
+{name: "五十嵐陽菜", have: false}, {name: "涼宮杏樹", have: false}, {name: "北見紗和子", have: false}, {name: "雛桃", have: false},
+{name: "藤本キララ", have: false}, {name: "かぐや姫", have: false}, {name: "エリサ", have: false}, {name: "寺崎千穂理", have: false},
+{name: "福姫", have: false}, {name: "七海礼奈", have: false}, {name: "姫川響", have: false}, {name: "森川綾子", have: false},
+{name: "小野寺七羽", have: false}, {name: "ゆず", have: false}, {name: "四宮冬実", have: false}, {name: "セイラン", have: false},
+{name: "如月彩音", have: false}, {name: "未来", have: false}, {name: "嵐星", have: false}, {name: "ハンナ", have: false},
+{name: "イブ・クリス", have: false}, {name: "琳琅", have: false}];
+
+const itemList = [{name: "和了演出:爆炎", have: false}, {name: "和了演出:烈風", have: false}, {name: "和了演出:桜吹雪", have: false},
+{name: "和了演出:黒炎", have: false}, {name: "和了演出:炎の嵐", have: false}, {name: "和了演出:電子", have: false},
+{name: "和了演出:赤い薔薇", have: false}, {name: "和了演出:ドットK.O.", have: false}, {name: "和了演出:逆鱗", have: false},
+{name: "和了演出:ゴースト", have: false}, {name: "和了演出:ブラックホール", have: false}, {name: "和了演出:切り裂き", have: false},
+{name: "和了演出:水の道", have: false}, {name: "和了演出:青炎", have: false}, {name: "和了演出:凍結", have: false},
+{name: "和了演出:火焔", have: false}, {name: "和了演出:残影", have: false}, {name: "和了演出:羽", have: false},
+{name: "和了演出:ドットコイン", have: false}, {name: "和了演出:龍舞", have: false}, {name: "和了演出:コウモリ", have: false},
+{name: "和了演出:ねこのサイン", have: false}, {name: "和了演出:心の鎌", have: false}, {name: "和了演出:雪玉転がし", have: false},
+{name: "にぼしの立直棒", have: false}, {name: "ねぎの立直棒", have: false}, {name: "骨の立直棒", have: false},
+{name: "クリムゾン立直棒", have: false}, {name: "24Kゴールド棒", have: false}, {name: "悪断ちの立直棒", have: false},
+{name: "アイスバーの立直棒", have: false}, {name: "小悪魔の立直棒", have: false}, {name: "硬いパンの立直棒", have: false},
+{name: "面白眼鏡の立直棒", have: false}, {name: "球児の立直棒", have: false}, {name: "ネオンサインの立直棒", have: false},
+{name: "エメラルドの雀卓", have: false}, {name: "鳩羽色の雀卓", have: false}, {name: "バイオレットの雀卓", have: false},
+{name: "スイカの雀卓", have: false}, {name: "芽吹きの地の雀卓", have: false}, {name: "花月夜の雀卓", have: false},
+{name: "ホタルの森の雀卓", have: false}, {name: "紫藤の雀卓", have: false}, {name: "ワインレッドの雀卓", have: false},
+{name: "ライトグレーの雀卓", have: false}, {name: "淡黄色の麻雀牌", have: false}, {name: "若葉色の麻雀牌", have: false},
+{name: "真っ赤な麻雀牌", have: false}, {name: "ゴーストの麻雀牌", have: false}, {name: "真珠貝の麻雀牌", have: false},
+{name: "みかんの麻雀牌", have: false}, {name: "しいたけの麻雀牌", have: false}, {name: "コスモスの麻雀牌", have: false},
+{name: "ローズグレーの麻雀牌", have: false}, {name: "青の麻雀牌", have: false}, {name: "子ネコの手", have: false},
+{name: "「真剣勝負」", have: false}, {name: "「激闘」", have: false}, {name: "「出陣」", have: false},
+{name: "「一触即発」", have: false}, {name: "「勝利への道」", have: false}, {name: "「凌雲の志」", have: false},
+{name: "秘伝の戦陣", have: false}, {name: "気ままに", have: false}, {name: "和やか", have: false},
+{name: "未来", have: false}];
+
+const rareMaterialList = [{name: "香ばしいクッキー"}, {name: "次世代ゲーム機"}, {name: "不朽の名画"},
+{name: "高級ワイングラス"}, {name: "碧洋のハート"}, {name: "テディベアXXL"},
+{name: "大手の同人誌"}, {name: "派手なワンピース"}];
+
+const materialList = [{name: "バタークッキー"}, {name: "携帯ゲーム機"}, {name: "立派な絵画"},
+{name: "上品なおちょこ"}, {name: "巨大なダイヤ"}, {name: "テディベアL"},
+{name: "薄い本"}, {name: "セーラー服"}];
+
+const havingCharTable = document.querySelector("#havingChar");
+const havingItemTable = document.querySelector("#havingItem");
+
+function makeTable(table, list) {
+    for ( let i = 0; i < list.length; i++) {
+        const havingDiv = document.createElement("div");
+        const havingImg = document.createElement("img");
+        const havingName = document.createElement("span");
+        havingDiv.classList.add("tableDiv");
+        havingImg.classList.add(`${table.id}Img`, "notHaving");
+        havingName.classList.add("tableSpan");
+        if (list === charList) {
+            havingImg.src = `img/char(${i}).png`;
+        } else {
+            havingImg.src = `img/item(${i}).png`;
+        }
+        havingName.innerText = list[i].name;
+        havingDiv.append(havingImg);
+        havingDiv.append(havingName);
+        table.append(havingDiv);
+    }
+}
+
+makeTable(havingCharTable, charList);
+makeTable(havingItemTable, itemList);
+
+let rollNum = 0;
+let wishStoneNum = 0;
+let starStoneNum = 0;
+
+const rollButton = document.querySelector("#roll");
+const resultSpace = document.querySelector("#resultSpace");
+const rollNumH1 = document.querySelector(".rollNum");
+const wishStone = document.querySelector(".stone_num .wish_stone span");
+const starStone = document.querySelector(".stone_num .star_stone span");
+
+async function clickButton() {
+    buttonActive(rollButton);
+    while (resultSpace.firstChild) {
+        resultSpace.removeChild(resultSpace.firstChild);
+    }
+    await tenRoll();
+    rollNumH1.innerText = `ガチャ回数: ${rollNum}回`;
+    confirmationHaving();
+}
+
+function makeRandNum() {
+    const randNum = Math.floor(Math.random() * 100);
+    if (randNum >= 95) {
+        const charNum = Math.floor(Math.random() * 34);
+        return {list: charList, num: charNum, url: `img/char(${charNum}).png`};
+    } else if (randNum >= 80) {
+        const itemNum = Math.floor(Math.random() * 67);
+        return {list: itemList, num: itemNum, url: `img/item(${itemNum}).png`};
+    }
+    const randNum1 = Math.floor(Math.random() * 10000);
+    if (randNum1 >= 9375) {
+        const rareMaterialNum = Math.floor(Math.random() * 8);
+        return {list: rareMaterialList, num: rareMaterialNum, url: `img/rare_material(${rareMaterialNum}).png`};
+    }
+    const materialNum = Math.floor(Math.random() * 8);
+    return {list: materialList, num: materialNum, url: `img/material(${materialNum}).png`};
+}
+
+function confirmationHaving() {
+    const tableImgChar = document.querySelectorAll(".havingCharImg");
+    const tableImgItem = document.querySelectorAll(".havingItemImg");
+    removeClass(charList, tableImgChar);
+    removeClass(itemList, tableImgItem);
+}
+
+function removeClass(list, imgs) {
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].have === true) {
+            imgs[i].classList.remove("notHaving");
+        }
+    }
+}
+
+function rollGacha() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const newElems = {
+                div: document.createElement("div"),
+                img: document.createElement("img"),
+                h2: document.createElement("h2")
+            };
+            addClass(newElems);
+            gachaResult(newElems);
+            newElems.div.append(newElems.img);
+            newElems.div.append(newElems.h2);
+            resultSpace.append(newElems.div);
+            resolve();
+        }, 300);
+    })
+}
+
+async function tenRoll() {
+    for (let i = 0; i < 10; i++) {
+        await rollGacha();
+        rollNum++;
+    }
+}
+
+function addClass({ div, img, h2 }) {
+    div.classList.add("resultStyle");
+    img.classList.add("gachaResultImg");
+    h2.classList.add("gachaResultName");
+}
+
+function gachaResult({ img, h2 }) {
+    const { list, num, url } = makeRandNum();
+    if (list[num].have === true) {
+        if (list === charList) {
+            img.src = "img/wishStone.png";
+            h2.innerText = "願いの石";
+            wishStoneNum++;
+            wishStone.innerText = `願いの石: ${wishStoneNum}個`;
+        } else {
+            img.src = "img/starStone.png";
+            h2.innerText = "星の石";
+            starStoneNum++;
+            starStone.innerText = `星の石: ${starStoneNum}個`;
+        }
+    } else {
+        if (list === charList || list === itemList) {
+            list[num].have = true;
+        }
+        img.src = url;
+        h2.innerText = list[num].name;
+    }
+}
+
+function buttonActive(button) {
+    button.classList.add("active");
+    button.innerText = "祈願中";
+    setTimeout(() => {
+        button.classList.remove("active");
+        button.innerText = "祈願！";
+    }, 3000)
+}
+
+rollButton.addEventListener("click", clickButton);
---
+++ jantama_anime.js
@@ -0,0 +1,94 @@
+const paiTyun = document.querySelector(".pai_tyun");
+const paiHatu = document.querySelector(".pai_hatu");
+const paiHaku =document.querySelector(".pai_haku");
+
+function makeDot(dotDiv) {
+    for (let i = 0; i < 264 ; i++) {
+        const dot = document.createElement("div");
+        dot.classList.add(`dot_${dotDiv.className}`);
+        dotDiv.append(dot);
+    }
+}
+
+makeDot(paiTyun);
+makeDot(paiHatu);
+makeDot(paiHaku);
+
+function anime1(dotDiv) {
+    let count12 = 0;
+    let count6Top = 0;
+    let count6Bottom = 4;
+    let count6Middle = 3;
+    setInterval(() => {
+        const dot = document.querySelectorAll(`.dot_${dotDiv.className}`);
+        function addBlack(i, count6) {
+            const count = (12 * count6);
+            if ((i > 12 + count && i < 23 + count) ||
+            (i > 192 + count && i < 203 + count) ||
+            (i >= 24 + count && i <= 180 + count && i % 12 === 0) ||
+            (i >= 35 + count && i <= 191 + count && i % 12 === 11)) {
+                dot[i].classList.add("black");
+            }
+        }
+        for (let i = 0; i < dot.length; i++) {
+            dot[i].classList.remove("black", "red", "green");
+            if (dotDiv.className === "pai_tyun") {
+                const count = (12 * count6Top);
+                if ((i >= 74 + count && i <= 80 + count) ||
+                (i >= 110 + count && i <= 116 + count) ||
+                (i >= 41 + count && i <= 173 + count && i % 12 === 5) ||
+                (i >= 74 + count && i <= 110 + count && i % 12 === 2) ||
+                (i >= 80 + count && i <= 126 + count && i % 12 === 8)) {
+                    dot[i].classList.add("red");
+                }
+                addBlack(i, count6Top);
+            } else if (dotDiv.className === "pai_hatu") {
+                const count = (12 * count6Bottom);
+                if ((i >= 50 + count && i <= 54 + count) ||
+                (i >= 78 + count && i <= 80 + count) ||
+                (i >= 91 + count && i <= 93 + count) ||
+                (i >= 98 + count && i <= 100 + count) ||
+                (i >= 102 + count && i <= 104 + count) ||
+                (i >= 122 + count && i <= 124 + count) ||
+                (i >= 126 + count && i <= 128 + count) ||
+                (i >= 138 + count && i <= 140 + count) ||
+                (i >= 146 + count && i <= 148 + count) ||
+                (i >= 170 + count && i <= 172 + count) ||
+                (i === 56 + count || i === 69 + count || i === 64 + count ||
+                i === 76 + count || i === 74 + count || i === 112 + count ||
+                i === 116 + count || i === 134 + count || i === 152 + count ||
+                i === 150 + count || i === 163 + count || i === 174 + count ||
+                i === 86 + count || i === 87 + count || i === 176 + count ||
+                i === 177 + count || i === 66 + count || i === 67 + count ||
+                i === 160 + count)) {
+                    dot[i].classList.add("green");
+                }
+                addBlack(i, count6Bottom);
+            } else {
+                addBlack(i, count6Middle);
+            }
+        }
+        if (count12 === 11) {
+            count12 = -1;
+            count6Top = 0;
+            count6Middle = 2;
+            count6Bottom = 4;
+        } else if (count12 >= 5) {
+            count6Top--;
+            count6Bottom++;
+        } else {
+            count6Top++;
+            count6Bottom--;
+        }
+        if (count12 >= 2 && count12 <= 7) {
+            count6Middle--;
+        } else {
+            count6Middle++;
+        }
+        count12++;
+    }, 200)
+}
+
+anime1(paiTyun);
+anime1(paiHatu);
+anime1(paiHaku);
